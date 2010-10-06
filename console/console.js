f3e116d5efbbe3eab022ffd78a956ff40319aac4
Pedro Rodrigues <pdfrodrigues@gmail.com>
2010-10-06 01:04:04 +0100

    Added documentation to console/console.js

--- console/console.js
+++ console/console.js
@@ -1,6 +1,11 @@
-replLoop();
+replLoop(); // Start the read-eval-print-loop
 
 
+/**
+ * Executes a read-eval-print-loop. The commands are read from the standard
+ * input, evaluated, and the result is outputted. Errors in the evaluation
+ * (e.g., syntax errors) are catched and reported.
+ */
 function replLoop()
 {
     while (true)
@@ -21,6 +26,13 @@ function replLoop()
 }
 
 
+/**
+ * Inspects the given object, outputting to the standard output a list of
+ * it's attributes and values.
+ *
+ * params:
+ *     obj - the object to be inspected
+ */
 function inspect(obj)
 {
     for ($key in obj)
